29104daabf9a510802d9f5acc584262b3464b75a
Kali-Venum <subhamdas.abcd@gmail.com>
2023-05-25 10:48:01 +0530

    working on splash screen, splash screen added.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 
 // Components.
@@ -6,8 +6,14 @@ import FlatCards from './components/FlatCards';
 import ElevatedCards from './components/ElevatedCards';
 import FancyCards from './components/FancyCards';
 import ActionCard from './components/ActionCard';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+
+  useEffect(() => {
+    SplashScreen.hide()
+  })
+
   return (
     <SafeAreaView>
       <ScrollView>
