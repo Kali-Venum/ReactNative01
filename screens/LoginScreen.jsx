70b6a906e7b28acf9968477e07fbd6cb2c14b438
Kali-Venum <subhamdas.abcd@gmail.com>
2023-05-25 20:56:59 +0530

    working on the login screen.

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,7 +1,17 @@
-import {Image, StyleSheet, Text, TextInput, View} from 'react-native';
-import React from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
 
 export default function LoginScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View>
       <View style={styles.headingView}>
@@ -14,7 +24,23 @@ export default function LoginScreen() {
         />
       </View>
       <View>
-        <TextInput style={styles.textInput} />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter your email"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter your password"
+          value={password}
+          onChangeText={setPassword}
+        />
+        <View>
+          <TouchableOpacity style={styles.buttonInput}>
+            <Text style={{color: 'white'}}>Login</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -36,5 +62,16 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
+    borderRadius: 50,
+  },
+  buttonInput: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#83a300',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 50,
   },
 });
